e4cce15f98183e47288bd8666a4b81a0bdf6a6f7
johnfn <johnfn@Grants-MacBook-Pro.local>
2021-10-01 21:29:04 -0700

    Add player scene

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var speed = 400.0
+
+func _ready():
+  pass 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  var dx = 0
+  var dy = 0
+  
+  dx += -1 if Input.is_action_pressed("ui_left") else 0
+  dx += 1 if Input.is_action_pressed("ui_right") else 0
+  
+  dy += -1 if Input.is_action_pressed("ui_up") else 0
+  dy += 1 if Input.is_action_pressed("ui_down") else 0
+  
+  var d = Vector2(dx, dy) * speed
+  
+  move_and_slide(d)
