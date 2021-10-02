038478ae02440dd68a0a17258a6782db27fd5fc7
gabrielle-delforge <gabrielle.delforge@berkeley.edu>
2021-10-02 16:28:33 -0700

    stuff

--- HealthBar.gd
+++ HealthBar.gd
@@ -6,17 +6,18 @@ var _hp: int;
 
 var MAX_HP_LENGTH_PX = 100;
 
-onready var color_rect = $'ColorRect'
+onready var health_bar = $'Inner'
+onready var border = $'Outer'
 
 func _init(max_hp = 50).():
   _max_hp = max_hp;
 
 func _ready():
-  color_rect.rect_size = Vector2(MAX_HP_LENGTH_PX, 10) 
-
+  health_bar.rect_size = Vector2(MAX_HP_LENGTH_PX, 10) 
+  border.rect_size = Vector2(MAX_HP_LENGTH_PX + 1, 10 + 1) 
 
 func set_hp(hp: int):
   var length = hp * MAX_HP_LENGTH_PX / _max_hp
   _hp = hp
-  color_rect.rect_size = Vector2(length, 10) 
+  health_bar.rect_size = Vector2(length, 10) 
  
