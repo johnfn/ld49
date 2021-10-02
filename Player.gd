8fcf0297a7e180dadc401a21acf1a6ddc7fca4e0
johnfn <johnfn@Grants-MacBook-Pro.local>
2021-10-02 14:13:40 -0700

    yeehaw

--- Player.gd
+++ Player.gd
@@ -20,18 +20,32 @@ func move():
   move_and_slide(d)
 
 func check_for_interactions():
-  if Input.is_action_just_pressed("action"):
-    var interactables = G.interactables 
-    var first = interactables[0]
-
-    first.interact()
+  var interactables = G.interactables
   
-  if Input.is_action_just_pressed("angry"):
-    var interactables = G.interactables
-    var first = interactables[0]
-    
+  var closest: Node2D = null
+  var closest_dist = 99999
+  
+  if interactables.size() == 0:
+    return
+  
+  for i in interactables:
+    if i.position.distance_to(position) < closest_dist:
+      closest_dist = i.position.distance_to(position)
+      closest = i
+  
+  # show target only for the one we're interacting w
+  
+  for i in interactables:
+    for child in i.get_children():
+      if child.is_in_group("interactable"):
+        child.visible = i == closest
+  
+  if Input.is_action_just_pressed("action"):
+    closest.interact()
+  
+  if Input.is_action_just_pressed("angry"):    
     G.in_battle = true
-    G.battling_against = [first.enemy_type, first.enemy_type]
+    G.battling_against = [closest.enemy_type, closest.enemy_type]
     get_tree().change_scene("res://Battle.tscn")
 
 func _process(delta):
