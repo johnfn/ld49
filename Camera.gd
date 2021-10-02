6d0a8002ee4bd41233e293e3b675e3a5859e9b23
johnfn <johnfn@Grants-MacBook-Pro.local>
2021-10-01 21:51:20 -0700

    Add interaction thingy

--- Camera.gd
+++ Camera.gd
@@ -3,10 +3,8 @@ extends Camera2D
 onready var player = $"/root/Main/Player"
 
 func _ready():
-  pass # Replace with function body.
+  pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
   position.x = player.position.x
   position.y = player.position.y
---
+++ InteractionMarker.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+onready var player: KinematicBody2D = $"/root/Main/Player"
+onready var marker = $InteractionMarker
+onready var animation = $AnimationPlayer
+
+var is_player_inside = false
+
+func _process(delta):
+  if is_player_inside:
+    if not marker.visible:
+      animation.play("Bobble")
+                                    
+    marker.visible = true
+  else:
+    marker.visible = false
+
+func _on_Area2D_body_entered(body):
+  if body == player:
+    is_player_inside = true
+
+func _on_Area2D_body_exited(body):
+  if body == player:
+    is_player_inside = false
