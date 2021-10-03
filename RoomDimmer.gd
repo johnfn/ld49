a10a78acc0dda07abe4c97c1a356e8300be3d574
johnfn <johnfn@Grants-MacBook-Pro.local>
2021-10-03 09:58:22 -0700

    Start out rooms dim

--- RoomDimmer.gd
+++ RoomDimmer.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+func _ready():
+  $Graphics.modulate = Color(1, 1, 1, 0.5)
+
 func _on_Area2D_body_entered(body):
   if body == G.player():
     $Graphics.modulate = Color(1, 1, 1, 1)
-
 
 func _on_Area2D_body_exited(body):
   if body == G.player():
