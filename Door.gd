5dea12eab2aecb35a139a16b3fde6ab89f2a75a6
johnfn <johnfn@Grants-MacBook-Pro.local>
2021-10-03 23:06:22 -0700

    Add hallpass logic back?

--- Door.gd
+++ Door.gd
@@ -5,25 +5,20 @@ var enemy_type = G.ENEMIES.Door
 var tries = 0
 
 func _on_InteractionMarker_on_interact():
-  var can_open = G.has_true_enlightenment()
+  var can_open = G.has_hallpass()
   
   if can_open:
-    #TODO: TRigger fight?
     G.dialog().start([
-      { "speaker": "You", "dialog": "The doors to the teachers lounge...", },
-   ])
+      { "speaker": "You", "dialog": "With my hallpass, no one will question me", },
+    ])
 
     queue_free()
     return
 
   tries += 1 
   
-  if tries == 1:
+  if tries >= 1:
     G.dialog().start([
-      { "speaker": "You", "dialog": "The door to the teacher's lounge is locked.", },
-      { "speaker": "You", "dialog": "Probably a good idea with students this belligerent.", },
-    ])
-  else:
-    G.dialog().start([
-      { "speaker": "You", "dialog": "The teacher's lounge doors are still locked.", },
+      { "speaker": "You", "dialog": "The door to the rest of the school.", },
+      { "speaker": "You", "dialog": "Miss Trunchbull won't let me leave...", },
     ])
