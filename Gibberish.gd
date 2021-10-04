3af3b881099744aa8977f99dc670b129d9995412
johnfn <johnfn@Grants-MacBook-Pro.local>
2021-10-04 01:00:38 -0700

    Gibberish

---
+++ Gibberish.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+func _ready():
+  play_deep_sound(10)
+
+func play_deep_sound(length = 5):
+  for x in range(length):
+    var rand = $Deep.get_children()[randi() % $Deep.get_children().size()]
+    rand.play()
+    
+    for y in range(7):
+      yield(get_tree(), "idle_frame")
+
+func play_timmy_sound(length = 5):
+  for x in range(length):
+    var rand = $Timmy.get_children()[randi() % $Timmy.get_children().size()]
+    rand.play()
+    
+    for y in range(7):
+      yield(get_tree(), "idle_frame")
