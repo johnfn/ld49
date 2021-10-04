af0d24c87615e5d2e5ff5357fb01af52f208fff6
johnfn <johnfn@Grants-MacBook-Pro.local>
2021-10-04 15:24:20 -0700

    qol fixes

--- InteractionMarker.gd
+++ InteractionMarker.gd
@@ -82,7 +82,12 @@ func check_for_interactions():
     choose_anger()
   
 func update_labels():
+  var text = Enemies.info()[interactor.enemy_type].interaction
+  
   interact_label.text = Enemies.info()[interactor.enemy_type].interaction
+  var eventual_width = interact_label.get_font("font").get_string_size(text).x
+  $Menu/InteractMenuItem.rect_size.x = eventual_width + 40
+
 
 func can_get_angry() -> bool:
   var info = Enemies.info()[interactor.enemy_type]
